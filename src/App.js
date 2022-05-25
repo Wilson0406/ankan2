521ee21d5f18b1bd39b8ac5b8b041c5be6bb4396
Ankan <ankankarmakar37@gmail.com>
2022-05-25 08:46:38 -0700

    footer part added

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import India from './pages/India';
+import Sports from './pages/Sports';
+import ScienceAndTech from './pages/ScienceAndTech';
+import Economy from './pages/Economy';
+import Politics from './pages/Politics';
+import International from './pages/International';
+import Governance from './pages/Governance';
+import Footer  from './Components/paros/components/Footer'
+function App() {
+
+
+  return (
+
+    <>
+    {/* .......................routing part......................... */}
+
+
+       <India></India>
+       {/* <Sports></Sports> */}
+       {/* <ScienceAndTech></ScienceAndTech> */}
+       {/* <Economy></Economy> */}
+       {/* <Politics></Politics> */}
+       {/* <International></International> */}
+       {/* <Governance></Governance> */}
+       
+       <Footer></Footer>
+       
+       
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/paros/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import like from './icons/like.svg';
+import "./Footer.css";
+
+function footer() {
+  return (
+    <div>
+      <div className='footer'>
+        <div className='image_paros'>
+          <img className="my-3 image_paros_img" src={like} alt='like' />
+        </div>
+        <div className='buttons_paros'>
+          <button className='button' >
+            <p><b>About</b></p>
+          </button>
+          <button className='button'>
+            <p><b>Contact</b></p>
+          </button>
+          <button className='button'>
+            <p><b>ValueAd</b></p>
+          </button>
+          <button className='button'>
+            <p><b>Careers</b></p>
+          </button>
+
+
+
+
+          <div className='lowerbutton_div'>
+            <button className='lowerbutton'>
+              <p><b>Terms of Use</b></p>
+            </button>
+            <button className='lowerbutton'>
+              <p><b>PrivacyPolicy</b></p>
+            </button>
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className='footerbelow' >
+        <p className='text' style={{ position: 'relative', top: 10, right: -200, bottom: 200, justifyContent: 'center', alignItems: 'center', fontSize: 10 }} >
+          <b>Copyright ©  2022 Printline Media Pvt. Ltd. All rights reserved.</b>
+        </p>
+        <div className='flex' style={{ position: 'relative', left: 20 }}>
+          <p style={{ position: 'relative', top: 10, left: 450, right: -200, bottom: 200, justifyContent: 'center', alignItems: 'center', fontSize: 10 }}> <b>FollowUs</b></p>
+          <div className='icons' style={{ position: 'relative', left: 470 , top:-8 }}>
+
+            <i className="fa-brands fa-facebook insta"></i>
+            <i className="fa-brands fa-instagram-square insta" ></i>
+            <i className="fa-brands fa-linkedin insta"></i>
+            <i className="fa-brands fa-telegram insta"></i>
+            <i className="fa-brands fa-twitter insta"></i>
+            <i className="fa-brands fa-youtube insta"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default footer
